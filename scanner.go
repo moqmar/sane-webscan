1fbcf7df83f5953feb4346e18445a64616ad9111
Moritz Marquardt <git@mo-mar.de>
2018-11-08 18:38:22 +0100

    Fix syntax error

--- scanner.go
+++ scanner.go
@@ -48,7 +48,7 @@ func scan(c *gin.Context) {
 
 	c.Header("Content-Type", "image/png")
 	c.Status(200)
-	err := png.Encode(c.Writer, image)
+	err = png.Encode(c.Writer, image)
 	if err != nil {
 		c.String(500, "Image Encoding Error: %s", err)
 	}

bd18d02ec323f71b4a2ba0b2ada8b8a3bfc4e382
Moritz Marquardt <git@mo-mar.de>
2018-11-09 16:41:10 +0100

    Fix file format

--- buttons.go
+++ buttons.go
@@ -13,12 +13,12 @@ func buttons() {
 		for device, connection := range con {
 			v, err := connection.GetOption("copy")
 			if err == nil && v.(bool) == true {
-				f, err := os.OpenFile("/tmp/sane-webscan-copy.pdf", os.O_CREATE|os.O_RDWR, 0600)
+				f, err := os.OpenFile("/tmp/sane-webscan-copy.png", os.O_CREATE|os.O_RDWR, 0600)
 				if err != nil {
 					log.Printf("Copy error: %s (OpenFile)\n", err)
 				}
 				doScan(device, f, url.Values{})
-				cmd := exec.Command("lp", "/tmp/sane-webscan-copy.pdf")
+				cmd := exec.Command("lp", "/tmp/sane-webscan-copy.png")
 				err = cmd.Run()
 				if err != nil {
 					log.Printf("Copy error: %s (lp)\n", err)

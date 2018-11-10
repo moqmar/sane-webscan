3ff9395a502921ca8a4035c35251081c8a0ac100
Moritz Marquardt <git@mo-mar.de>
2018-11-10 16:24:03 +0100

    Fix copying

--- buttons.go
+++ buttons.go
@@ -4,7 +4,7 @@ import (
 	"image"
 	"io"
 	"log"
-	"os"
+	"os/exec"
 	"time"
 
 	"golang.org/x/image/tiff"
@@ -15,13 +15,13 @@ func buttons() {
 		for device, connection := range con {
 			v, err := connection.GetOption("copy")
 			if err == nil && v.(bool) == true {
-				//cmd := exec.Command("lp", "-o", "portrait", "-o", "fit-to-page", "-o", "media=iso_a4_210x297mm")
-				//r, w := io.Pipe()
-				//cmd.Stdin = r
-				w := os.OpenFile("/tmp/sane-webscan-copy.tif", os.O_CREATE|os.O_RDWR, 0600)
+				cmd := exec.Command("lp", "-o", "portrait", "-o", "fit-to-page", "-o", "media=iso_a4_210x297mm", "-")
+				r, w := io.Pipe()
+				cmd.Stdin = r
+				//w, _ := os.OpenFile("/tmp/sane-webscan-copy.tif", os.O_CREATE|os.O_RDWR, 0600)
 				err := doScan(device, w, map[string]interface{}{}, func(w io.Writer, m image.Image) error {
 					return tiff.Encode(w, m, &tiff.Options{
-						Compression: tiff.Uncompressed,
+						Compression: tiff.Deflate,
 						Predictor:   true,
 					})
 				})
